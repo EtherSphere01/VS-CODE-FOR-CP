61260401818306cd8409861ed61db5d6480a1340
Md. Naimur Rahman <EtherSphere01>
2025-02-18 00:05:21 +0600

    codeforces

--- B_Variety_is_Discouraged.cpp
+++ B_Variety_is_Discouraged.cpp
@@ -1,234 +1,200 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <list>
+#include <set>
+#include <queue>
 #include <bits/stdc++.h>
-
-using namespace std;
-
-#define fastio()                      \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define sp " "
+#define ll long long
+#define int long long
 #define pb push_back
 #define ppb pop_back
 #define mp make_pair
+using namespace std;
+using vpi = vector<pair<int, int>>;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
 #define ff first
 #define ss second
-#define PI 3.141592653589793238462
-#define andd &&
-#define orr ||
-#define nott !
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define allr(x) (x).rbegin(), (x).rend()
-#define mem1(a) memset(a, -1, sizeof(a))
-#define mem0(a) memset(a, 0, sizeof(a))
-#define yes cout << "YES" << nline
-#define no cout << "NO" << nline
-#define yesno(f) \
-    if (f)       \
-        yes;     \
-    else         \
-        no
-#define noyes(f) \
-    if (!f)      \
-        yes;     \
-    else         \
-        no
-#define out(x) cout << x << nline
-#define rep(i, a, b) for (ll i = a; i < b; i++)
-#define rev(i, a, b) for (ll i = a; i >= b; i--)
-#define show(v, s, n)          \
-    for (ll i = s; i < n; i++) \
-        cout << v[i] << sp;
-#define show_rev(v, s, n)       \
-    for (ll i = s; i >= n; i--) \
-        cout << v[i] << sp;
-#define show_pair(v, s, n)     \
-    for (ll i = s; i < n; i++) \
-        cout << v[i].ff << sp << v[i].ss << nline;
-#define vl vector<ll>
-#define vi vector<int>
-#define vc vector<char>
-#define vs vector<string>
-#define vpair vector<pair<ll, ll>>
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
-
-#ifndef ONLINE_JUDGE
-#define debug(x)       \
-    cerr << #x << " "; \
-    _print(x);         \
-    cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
-
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <class T, class V>
-void _print(pair<T, V> p)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+const int inf = 9e18;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 10000000;
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+template <typename T, typename Y>
+istream &operator>>(istream &is, pair<T, Y> &p)
 {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
+    is >> p.first >> p.second;
+    return is;
 }
-template <class T>
-void _print(vector<T> v)
+template <typename T, typename Y>
+ostream &operator<<(ostream &os, pair<T, Y> p)
 {
-    cerr << "[ ";
-    for (T i : v)
+    os << p.first << ' ' << p.second << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+vector<int> lp, sieve;
+vector<int> pr;
+vector<int> power;
+vector<int> fact;
+void initpow(int x)
+{
+    power.resize(NUM);
+    power[0] = 1;
+    for (int i = 1; i < NUM; i++)
     {
-        _print(i);
-        cerr << " ";
+        power[i] = (power[i - 1] * (x % mod)) % mod;
     }
-    cerr << "]";
 }
-template <class T>
-void _print(set<T> v)
+void initFactorial()
 {
-    cerr << "[ ";
-    for (T i : v)
+    fact.resize(NUM);
+    fact[0] = 1;
+    for (int i = 1; i < NUM; i++)
     {
-        _print(i);
-        cerr << " ";
+        fact[i] = (fact[i - 1] * i) % mod;
     }
-    cerr << "]";
 }
-template <class T>
-void _print(multiset<T> v)
+void calc_sieve()
 {
-    cerr << "[ ";
-    for (T i : v)
+    sieve.resize(NUM + 1, 0);
+    for (int x = 2; x <= NUM; x++)
     {
-        _print(i);
-        cerr << " ";
+        if (sieve[x])
+            continue;
+        for (int u = x; u <= NUM; u += x)
+        {
+            sieve[u] = x;
+        }
     }
-    cerr << "]";
 }
-template <class T, class V>
-void _print(map<T, V> v)
+void primefactor()
 {
-    cerr << "[ ";
-    for (auto i : v)
+    lp.resize(N + 1, 0);
+    for (int i = 2; i <= N; ++i)
     {
-        _print(i);
-        cerr << " ";
+        if (lp[i] == 0)
+        {
+            lp[i] = i;
+            pr.push_back(i);
+        }
+        for (int j = 0; j < (int)pr.size() && pr[j] <= lp[i] && i * pr[j] <= N; ++j)
+            lp[i * pr[j]] = pr[j];
     }
-    cerr << "]";
 }
-
+int binpow(int a, int b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+int binpow(int a, int b, int mod)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res % mod;
+}
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+int lcm(int a, int b)
+{
+    return ((a / gcd(a, b)) * b);
+}
+int inversemod(int a, int mod)
+{
+    return binpow(a, mod - 2, mod);
+}
+int divmod(int a, int b, int c)
+{
+    return ((a % c) * inversemod(b, c)) % c;
+}
+int combination(int n, int k)
+{
+    if (k > n)
+        return 0;
+    int p1 = (fact[n] * inversemod(fact[k], mod)) % mod;
+    int p2 = (1 * inversemod(fact[n - k], mod)) % mod;
+    return (p1 * p2) % mod;
+}
 void solve()
 {
-
     ll n;
     cin >> n;
-    vl a(n);
-    map<ll, ll> unique;
+    vi v(n);
+    cin >> v;
+    map<ll, ll> f;
+    for (auto &&i : v)
+    {
+        f[i]++;
+    }
+    ll r = 0;
+    ll ansl = -1, ansr = -1;
+    ll len = 0;
     for (ll i = 0; i < n; i++)
     {
-        cin >> a[i];
-        unique[a[i]]++;
-    }
-
-    if (n == 1)
-    {
-        cout << 1 << sp << 1 << nline;
-        return;
-    }
-
-    ll temp = unique.size();
-    ll temp2 = 0;
-    ll k = 0;
-    for (auto i : unique)
-    {
-        k = max(k, i.second);
-    }
-    if (k == n)
-    {
-        cout << 0 << nline;
-        return;
-    }
-
-    ll ans1 = n - temp;
-    ll ans2 = 0;
-    ll left = 0, right = 0;
-    ll finalL, finalR;
-    for (ll i = 0; i < n; i++)
-    {
-        if (unique[a[i]] == 1)
+        r = max(r, i);
+        while (r < n && f[v[r]] == 1)
         {
-            right = i;
+            r++;
         }
-        else
+        if (r - i > len)
         {
-            ll t = right - left + 1;
-            ll p = n - t;
-            p = p - (temp - t);
-            if (p >= ans1)
-            {
-                ans1 = p;
-                finalL = left;
-                finalR = right;
-            }
-            left = right;
+            len = r - i;
+            ansl = i;
+            ansr = r - 1;
         }
     }
-
-    if (left != right)
+    if (ansl == -1)
     {
-        ll t = right - left + 1;
-        ll p = n - t;
-        p = p - (temp - t);
-        if (p >= ans1)
-        {
-            ans1 = p;
-            finalL = left;
-            finalR = right;
-        }
+        cout << 0 << endl;
     }
-
-    cout << finalL + 1 << sp << finalR + 1 << nline;
+    else
+    {
+        cout << ansl + 1 << " " << ansr + 1 << endl;
+    }
 }
-
-int main()
+signed main()
 {
-    fastio();
-
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
-#endif
-
-    int t;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
     cin >> t;
     while (t--)
         solve();
-
-#ifndef ONLINE_JUDGE
-    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
-#endif
+    return 0;
 }
--- C_Remove_the_Ends.cpp
+++ C_Remove_the_Ends.cpp
@@ -1,261 +1,198 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <list>
+#include <set>
+#include <queue>
 #include <bits/stdc++.h>
-
-using namespace std;
-
-#define fastio()                      \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define sp " "
+#define ll long long
+#define int long long
 #define pb push_back
 #define ppb pop_back
 #define mp make_pair
+using namespace std;
+using vpi = vector<pair<int, int>>;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
 #define ff first
 #define ss second
-#define PI 3.141592653589793238462
-#define andd &&
-#define orr ||
-#define nott !
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define allr(x) (x).rbegin(), (x).rend()
-#define mem1(a) memset(a, -1, sizeof(a))
-#define mem0(a) memset(a, 0, sizeof(a))
-#define yes cout << "YES" << nline
-#define no cout << "NO" << nline
-#define yesno(f) \
-    if (f)       \
-        yes;     \
-    else         \
-        no
-#define noyes(f) \
-    if (!f)      \
-        yes;     \
-    else         \
-        no
-#define out(x) cout << x << nline
-#define rep(i, a, b) for (ll i = a; i < b; i++)
-#define rev(i, a, b) for (ll i = a; i >= b; i--)
-#define show(v, s, n)          \
-    for (ll i = s; i < n; i++) \
-        cout << v[i] << sp;
-#define show_rev(v, s, n)       \
-    for (ll i = s; i >= n; i--) \
-        cout << v[i] << sp;
-#define show_pair(v, s, n)     \
-    for (ll i = s; i < n; i++) \
-        cout << v[i].ff << sp << v[i].ss << nline;
-#define vl vector<ll>
-#define vi vector<int>
-#define vc vector<char>
-#define vs vector<string>
-#define vpair vector<pair<ll, ll>>
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
-
-#ifndef ONLINE_JUDGE
-#define debug(x)       \
-    cerr << #x << " "; \
-    _print(x);         \
-    cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
-
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <class T, class V>
-void _print(pair<T, V> p)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+const int inf = 9e18;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 10000000;
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+template <typename T, typename Y>
+istream &operator>>(istream &is, pair<T, Y> &p)
 {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
+    is >> p.first >> p.second;
+    return is;
 }
-template <class T>
-void _print(vector<T> v)
+template <typename T, typename Y>
+ostream &operator<<(ostream &os, pair<T, Y> p)
 {
-    cerr << "[ ";
-    for (T i : v)
+    os << p.first << ' ' << p.second << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+vector<int> lp, sieve;
+vector<int> pr;
+vector<int> power;
+vector<int> fact;
+void initpow(int x)
+{
+    power.resize(NUM);
+    power[0] = 1;
+    for (int i = 1; i < NUM; i++)
     {
-        _print(i);
-        cerr << " ";
+        power[i] = (power[i - 1] * (x % mod)) % mod;
     }
-    cerr << "]";
 }
-template <class T>
-void _print(set<T> v)
+void initFactorial()
 {
-    cerr << "[ ";
-    for (T i : v)
+    fact.resize(NUM);
+    fact[0] = 1;
+    for (int i = 1; i < NUM; i++)
     {
-        _print(i);
-        cerr << " ";
+        fact[i] = (fact[i - 1] * i) % mod;
     }
-    cerr << "]";
 }
-template <class T>
-void _print(multiset<T> v)
+void calc_sieve()
 {
-    cerr << "[ ";
-    for (T i : v)
+    sieve.resize(NUM + 1, 0);
+    for (int x = 2; x <= NUM; x++)
     {
-        _print(i);
-        cerr << " ";
+        if (sieve[x])
+            continue;
+        for (int u = x; u <= NUM; u += x)
+        {
+            sieve[u] = x;
+        }
     }
-    cerr << "]";
 }
-template <class T, class V>
-void _print(map<T, V> v)
+void primefactor()
 {
-    cerr << "[ ";
-    for (auto i : v)
+    lp.resize(N + 1, 0);
+    for (int i = 2; i <= N; ++i)
     {
-        _print(i);
-        cerr << " ";
+        if (lp[i] == 0)
+        {
+            lp[i] = i;
+            pr.push_back(i);
+        }
+        for (int j = 0; j < (int)pr.size() && pr[j] <= lp[i] && i * pr[j] <= N; ++j)
+            lp[i * pr[j]] = pr[j];
     }
-    cerr << "]";
 }
-
+int binpow(int a, int b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+int binpow(int a, int b, int mod)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res % mod;
+}
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+int lcm(int a, int b)
+{
+    return ((a / gcd(a, b)) * b);
+}
+int inversemod(int a, int mod)
+{
+    return binpow(a, mod - 2, mod);
+}
+int divmod(int a, int b, int c)
+{
+    return ((a % c) * inversemod(b, c)) % c;
+}
+int combination(int n, int k)
+{
+    if (k > n)
+        return 0;
+    int p1 = (fact[n] * inversemod(fact[k], mod)) % mod;
+    int p2 = (1 * inversemod(fact[n - k], mod)) % mod;
+    return (p1 * p2) % mod;
+}
 void solve()
 {
-
     ll n;
     cin >> n;
-    vl a(n);
+    vector<ll> v;
     for (ll i = 0; i < n; i++)
-        cin >> a[i];
-    ll ans = 0;
-    ll left = 0, right = n - 1;
-
-    if (n == 1)
     {
-        out(abs(a[0]));
-        return;
+        ll a;
+        cin >> a;
+        v.push_back(a);
     }
-
-    while (left <= right)
+    vi pos(n, 0);
+    vi neg(n, 0);
+    ll possum = 0;
+    ll negsum = 0;
+    for (ll i = 0; i < n; i++)
     {
-        if (left == right)
+        if (v[i] > 0)
         {
-            if (a[left] > 0)
-            {
-                ans += abs(a[left]);
-            }
-            else if (a[right] < 0)
-            {
-                ans += a[left];
-            }
-            left++;
-            right--;
-            break;
+            possum += v[i];
         }
         else
         {
-            if (a[left] > 0)
-            {
-                ans += a[left];
-                left++;
-            }
-            if (a[right] < 0)
-            {
-                ans += abs(a[right]);
-                right--;
-            }
-
-            if (a[left] < 0 and a[right] > 0)
-            {
-                break;
-            }
+            negsum -= v[i];
         }
+        pos[i] = possum;
+        neg[i] = negsum;
     }
-
-    if (left > right)
+    ll ans = max(possum, negsum);
+    for (ll i = 0; i < n; i++)
     {
-        out(ans);
-        return;
+        ans = max(ans, pos[i] + negsum - neg[i]);
     }
-
-    ll leftans = 0, rightans = 0;
-    ll templeftans = 0, temprightans = 0;
-    ll leftuntill = left;
-    ll rightuntill = right;
-    for (ll i = left; i <= right; i++)
-    {
-        if (a[i] > 0)
-        {
-            leftans += templeftans;
-            templeftans = 0;
-        }
-        else
-        {
-            templeftans += abs(a[i]);
-        }
-    }
-    for (ll i = right; i >= left; i--)
-    {
-        if (a[i] < 0)
-        {
-            rightans += temprightans;
-            temprightans = 0;
-        }
-        else
-        {
-            temprightans += a[i];
-        }
-    }
-    if (templeftans != 0)
-    {
-        leftans += templeftans;
-    }
-    if (temprightans != 0)
-    {
-        rightans += temprightans;
-    }
-
-    ans += max(leftans, rightans);
-    out(ans);
+    cout << ans << endl;
 }
-
-int main()
+signed main()
 {
-    fastio();
-
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
-#endif
-
-    int t;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
     cin >> t;
     while (t--)
         solve();
-
-#ifndef ONLINE_JUDGE
-    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
-#endif
+    return 0;
 }
