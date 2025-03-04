08719270af6d263dbb7dc23b748f399a7351c16f
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-03-04 16:50:31 +0600

    'cf'

--- C_Limited_Repainting.cpp
+++ C_Limited_Repainting.cpp
@@ -86,12 +86,9 @@ void _print(ull t) { cerr << t; }
 
 template <class T, class V>
 void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
+template <class T>void _print(vector<T> v);
+template <class T>void _print(set<T> v);
+template <class T, class V>void _print(map<T, V> v);
 template <class T>
 void _print(multiset<T> v);
 template <class T, class V>
