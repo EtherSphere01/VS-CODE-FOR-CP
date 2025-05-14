fa0882412a4323db933b98b8da0ad40b56e4b4c7
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-05-14 12:21:20 +0600

    mac

--- ICPC/A.cpp
+++ ICPC/A.cpp
@@ -154,14 +154,36 @@ void solve()
 
     ll n;
     cin >> n;
-    vl a(n);
-    in(a);
-    ll ans = a[0];
-    for (ll i = 1; i < n; i++)
-    {
-        ans = ans & a[i];
+    vl a(n+1);
+    vl b(n+1);
+    for (ll i = 1; i < n; i++){
+        cin >> b[i];
     }
-    cout << ans << nline;
+        b[0] = 0, b[n] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        a[i] = b[i - 1] | b[i];
+    }
+    bool isTrue = true;
+    for (int i = 1; i < n; i++)
+
+        if ((a[i] & a[i + 1]) != b[i])
+        {
+            isTrue = false;
+            break;
+        }
+    if (isTrue)
+    {
+        for (ll i = 1; i <= n;i++){
+            cout << a[i] << sp;
+        }
+            cout << nline;
+    }
+    else
+    {
+        out(-1);
+    }
 }
 
 int main()
