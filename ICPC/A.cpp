9ae52cf588cbbf74824995ecf1c60d4ca3198670
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-05-21 12:13:30 +0600

    mac

--- ICPC/A.cpp
+++ ICPC/A.cpp
@@ -1,8 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout.precision(numeric_limits<double>::max_digits10);
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);                   \
+    cout.precision(numeric_limits<double>::max_digits10);
 #define MOD 1000000007
 #define MOD1 998244353
 #define INF 1e18
@@ -21,25 +25,43 @@ using namespace std;
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 #define allr(x) (x).rbegin(), (x).rend()
-#define mem1(a) memset(a,-1,sizeof(a))
-#define mem0(a) memset(a,0,sizeof(a))
+#define mem1(a) memset(a, -1, sizeof(a))
+#define mem0(a) memset(a, 0, sizeof(a))
 #define yes cout << "YES" << nline
 #define no cout << "NO" << nline
-#define yesno(f) if(f) yes; else no
-#define noyes(f) if(!f) yes; else no
+#define yesno(f) \
+    if (f)       \
+        yes;     \
+    else         \
+        no
+#define noyes(f) \
+    if (!f)      \
+        yes;     \
+    else         \
+        no
 #define out(x) cout << x << nline
-#define rep(i, a, b) for(ll i=a; i<b; i++)
-#define rev(i, a, b) for(ll i=a; i>=b; i--)
-#define in(a) for (auto &x : a) cin >> x;
-#define showr(v,s,n) for(ll i=s; i<n; i++) cout<<v[i]<<sp;
-#define show(v) for(auto i:v) cout<<i<<sp;
-#define show_rev(v,s,n) for(ll i=s; i>=n; i--) cout<<v[i]<<sp;
-#define show_pair(v,s,n) for(ll i=s; i<n; i++) cout<<v[i].ff<<sp<<v[i].ss<<nline;
-#define vl vector<ll> 
-#define vi vector<int> 
-#define vc vector<char> 
-#define vs vector<string> 
-#define vpair vector<pair<ll,ll>>
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define rev(i, a, b) for (ll i = a; i >= b; i--)
+#define in(a)         \
+    for (auto &x : a) \
+        cin >> x;
+#define showr(v, s, n)         \
+    for (ll i = s; i < n; i++) \
+        cout << v[i] << sp;
+#define show(v)      \
+    for (auto i : v) \
+        cout << i << sp;
+#define show_rev(v, s, n)       \
+    for (ll i = s; i >= n; i--) \
+        cout << v[i] << sp;
+#define show_pair(v, s, n)     \
+    for (ll i = s; i < n; i++) \
+        cout << v[i].ff << sp << v[i].ss << nline;
+#define vl vector<ll>
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vpair vector<pair<ll, ll>>
 
 typedef long long ll;
 typedef unsigned long long ull;
@@ -47,54 +69,127 @@ typedef long double lld;
 // typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 
 #ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
 #else
 #define debug(x)
 #endif
 
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
 
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-
-
-void solve() {
-
-
-
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
 }
 
-int main() {
+bool kth_bit_on(ll n, ll k)
+{
+    return ((n >> k) & 1);
+}
+
+void solve()
+{
+
+    ll x;
+    cin >> x;
+    ll msb = __lg(x);
+    ll b = (1 << msb);
+    ll a = (x ^ b);
+    ll sum = b, cnt = 0;
+
+    for (ll i = 0; i <= msb; i++)
+    {
+        if (!kth_bit_on(a, i) and !kth_bit_on(b, i) and (sum + (1 << i) <= x))
+        {
+            cnt++;
+            sum += (1 << i);
+        }
+    }
+    ll ans = (1 << cnt);
+    out(ans);
+}
+
+int main()
+{
     fastio();
 
 #ifndef ONLINE_JUDGE
     freopen("Error.txt", "w", stderr);
 #endif
 
-
-    int t; cin >> t;
-    while (t--) solve();
-
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
 
 #ifndef ONLINE_JUDGE
-    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC)*0.001 << "s\n";
+    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
 #endif
-
 }
-
-
-
