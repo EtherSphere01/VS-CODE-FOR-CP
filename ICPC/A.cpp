36d848c07e3135f47976e423260fc09dde91f78a
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-06-04 22:46:54 +0600

    hotash

--- ICPC/A.cpp
+++ ICPC/A.cpp
@@ -193,6 +193,3 @@ int main()
     cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
 #endif
 }
-
-// Time Complexity: O(log n) for each test case
-// Space Complexity: O(1) for each test case
