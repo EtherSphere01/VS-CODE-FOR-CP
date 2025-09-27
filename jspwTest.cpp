489f0a1d45ba37b95f52f187b9c0a5ae68d96e05
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-09-27 23:52:58 +0600

    icpc

--- jspwTest.cpp
+++
@@ -1,9 +0,0 @@
-Prime factorization of 15 is: 3 5 
-Prime factorization of 21 is: 3 7 
-Prime factorization of 66 is: 2 3 11 
-Prime factorization of 16 is: 2 2 2 2 
-Prime factorization of 33 is: 3 11 
-Prime factorization of 49 is: 7 7 
-Prime factorization of 25 is: 5 5 
-Prime factorization of 1000 is: 2 2 2 5 5 5 
-Prime factorization of 99991 is: 99991 
