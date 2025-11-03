701111e177273c0f74b99f9add5feae3152e1e51
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-11-03 20:56:08 +0600

    hotash

---
+++ M_M.cpp
@@ -0,0 +1,209 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);                   \
+    cout.precision(numeric_limits<double>::max_digits10);
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define sp " "
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define andd &&
+#define orr ||
+#define nott !
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define mem1(a) memset(a, -1, sizeof(a))
+#define mem0(a) memset(a, 0, sizeof(a))
+#define yes cout << "YES" << nline
+#define no cout << "NO" << nline
+#define yesno(f) \
+    if (f)       \
+        yes;     \
+    else         \
+        no
+#define noyes(f) \
+    if (!f)      \
+        yes;     \
+    else         \
+        no
+#define out(x) cout << x << nline
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define rev(i, a, b) for (ll i = a; i >= b; i--)
+#define in(a)         \
+    for (auto &x : a) \
+        cin >> x;
+#define showr(v, s, n)         \
+    for (ll i = s; i < n; i++) \
+        cout << v[i] << sp;
+#define show(v)      \
+    for (auto i : v) \
+        cout << i << sp;
+#define show_rev(v, s, n)       \
+    for (ll i = s; i >= n; i--) \
+        cout << v[i] << sp;
+#define show_pair(v, s, n)     \
+    for (ll i = s; i < n; i++) \
+        cout << v[i].ff << sp << v[i].ss << nline;
+#define vl vector<ll>
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vpair vector<pair<ll, ll>>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<long long> s(n);
+    for (int i = 0; i < n; ++i)
+        cin >> s[i];
+
+    // Compute consecutive gaps
+    int m = n - 1;
+    vector<long long> d(m);
+    for (int i = 0; i < m; ++i)
+        d[i] = s[i + 1] - s[i];
+
+    // Prefix and suffix maxima over gaps
+    vector<long long> pref(m), suf(m);
+    pref[0] = d[0];
+    for (int i = 1; i < m; ++i)
+        pref[i] = max(pref[i - 1], d[i]);
+    suf[m - 1] = d[m - 1];
+    for (int i = m - 2; i >= 0; --i)
+        suf[i] = max(suf[i + 1], d[i]);
+
+    debug(d);
+    debug(pref);
+    debug(suf);
+
+    long long ans = LLONG_MAX;
+    // Remove exactly one stop: index k in [1, n-2]
+    for (int k = 1; k <= n - 2; ++k)
+    {
+        long long other = 0;
+        if (k - 2 >= 0)
+            other = max(other, pref[k - 2]);
+        if (k + 1 <= m - 1)
+            other = max(other, suf[k + 1]);
+        long long merged = d[k - 1] + d[k];
+        long long cand = max(other, merged);
+        ans = min(ans, cand);
+    }
+
+    cout << ans << nline;
+}
+
+int main()
+{
+    fastio();
+
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    solve();
+
+#ifndef ONLINE_JUDGE
+    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
+#endif
+}
