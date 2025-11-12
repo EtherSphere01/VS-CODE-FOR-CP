7d9a8e64a9458e8516429c797d526cbbd48d7197
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-11-12 23:48:21 +0600

    hotash

--- Increasing_Array.cpp
+++ Increasing_Array.cpp
@@ -1,185 +1,104 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-#define fastio()                      \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define sp " "
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define andd &&
-#define orr ||
-#define nott !
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define allr(x) (x).rbegin(), (x).rend()
-#define mem1(a) memset(a, -1, sizeof(a))
-#define mem0(a) memset(a, 0, sizeof(a))
-#define yes cout << "YES" << nline
-#define no cout << "NO" << nline
-#define yesno(f) \
-    if (f)       \
-        yes;     \
-    else         \
-        no
-#define noyes(f) \
-    if (!f)      \
-        yes;     \
-    else         \
-        no
-#define out(x) cout << x << nline
-#define rep(i, a, b) for (ll i = a; i < b; i++)
-#define rev(i, a, b) for (ll i = a; i >= b; i--)
-#define in(a)         \
-    for (auto &x : a) \
-        cin >> x;
-#define showr(v, s, n)         \
-    for (ll i = s; i < n; i++) \
-        cout << v[i] << sp;
-#define show(v)      \
-    for (auto i : v) \
-        cout << i << sp;
-#define show_rev(v, s, n)       \
-    for (ll i = s; i >= n; i--) \
-        cout << v[i] << sp;
-#define show_pair(v, s, n)     \
-    for (ll i = s; i < n; i++) \
-        cout << v[i].ff << sp << v[i].ss << nline;
-#define vl vector<ll>
-#define vi vector<int>
-#define vc vector<char>
-#define vs vector<string>
-#define vpair vector<pair<ll, ll>>
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
-
-#ifndef ONLINE_JUDGE
-#define debug(x)       \
-    cerr << #x << " "; \
-    _print(x);         \
-    cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
-
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <class T, class V>
-void _print(pair<T, V> p)
+class Node
 {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <class T>
-void _print(vector<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
+public:
+    int data;
+    Node *next;
+    Node(int val)
     {
-        _print(i);
-        cerr << " ";
+        data = val;
+        next = nullptr;
     }
-    cerr << "]";
-}
-template <class T>
-void _print(set<T> v)
+};
+
+void printLL(Node *head)
 {
-    cerr << "[ ";
-    for (T i : v)
+    Node *temp = head;
+    while (temp)
     {
-        _print(i);
-        cerr << " ";
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    cerr << "]";
-}
-template <class T>
-void _print(multiset<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T, class V>
-void _print(map<T, V> v)
-{
-    cerr << "[ ";
-    for (auto i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
+
+    cout << endl;
 }
 
-void solve()
+Node *insert_at_head(Node *head, int data)
 {
-
-    ll n;
-    cin >> n;
-    vl a(n);
-    in(a);
-
-    ll sum = 0;
-    for (ll i = 0; i < n - 1; i++)
+    Node *newNode = new Node(data);
+    if (head == nullptr)
     {
-        if (a[i] > a[i + 1])
+        return newNode;
+    }
+
+    newNode->next = head;
+    return newNode;
+}
+
+Node *insert_at_tail(Node *head, int data)
+{
+    Node *newNode = new Node(data);
+    if (head == nullptr)
+    {
+        return newNode;
+    }
+
+    Node *temp = head;
+
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return head;
+}
+
+Node *insert_at_mid(Node *head, int pos, int data)
+{
+    Node *newNode = new Node(data);
+
+    if (pos == 1)
+    {
+        return insert_at_head(head, data);
+    }
+
+    int count = 0;
+    Node *temp = head;
+    Node *prev = head;
+    while (temp)
+    {
+        count++;
+        if (count == pos)
         {
-            sum += a[i] - a[i + 1];
-            a[i + 1] = a[i];
+            newNode->next = prev->next;
+            prev->next = newNode;
+        }
+        else
+        {
+            prev = temp;
+            temp = temp->next;
         }
     }
 
-    out(sum);
+    return head;
 }
 
 int main()
 {
-    fastio();
+    vector<int> arr = {1, 2, 2, 3, 4, 4, 5};
+    Node *head = new Node(arr[0]);
+    int n = arr.size();
 
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
-#endif
+    Node *temp = head;
+    for (int i = 1; i < n; i++)
+    {
+        Node *newNode = new Node(arr[i]);
+        temp->next = newNode;
+        temp = newNode;
+    }
 
-    solve();
-
-#ifndef ONLINE_JUDGE
-    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
-#endif
+    printLL(head);
+    head = insert_at_mid(head, 7, 100);
+    printLL(head);
 }
