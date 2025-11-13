6f8f657c73847acee164eea370b4819fc24fc885
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-11-13 09:38:59 +0600

    hotash

--- C_Symmetrical_Polygons.cpp
+++ C_Symmetrical_Polygons.cpp
@@ -1,246 +1,80 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+#define int long long int
 
-#define fastio()                      \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);                   \
-    cout.precision(numeric_limits<double>::max_digits10);
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define sp " "
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define andd &&
-#define orr ||
-#define nott !
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define allr(x) (x).rbegin(), (x).rend()
-#define mem1(a) memset(a, -1, sizeof(a))
-#define mem0(a) memset(a, 0, sizeof(a))
-#define yes cout << "YES" << nline
-#define no cout << "NO" << nline
-#define yesno(f) \
-    if (f)       \
-        yes;     \
-    else         \
-        no
-#define noyes(f) \
-    if (!f)      \
-        yes;     \
-    else         \
-        no
-#define out(x) cout << x << nline
-#define rep(i, a, b) for (ll i = a; i < b; i++)
-#define rev(i, a, b) for (ll i = a; i >= b; i--)
-#define in(a)         \
-    for (auto &x : a) \
-        cin >> x;
-#define showr(v, s, n)         \
-    for (ll i = s; i < n; i++) \
-        cout << v[i] << sp;
-#define show(v)      \
-    for (auto i : v) \
-        cout << i << sp;
-#define show_rev(v, s, n)       \
-    for (ll i = s; i >= n; i--) \
-        cout << v[i] << sp;
-#define show_pair(v, s, n)     \
-    for (ll i = s; i < n; i++) \
-        cout << v[i].ff << sp << v[i].ss << nline;
-#define vl vector<ll>
-#define vi vector<int>
-#define vc vector<char>
-#define vs vector<string>
-#define vpair vector<pair<ll, ll>>
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
-
-#ifndef ONLINE_JUDGE
-#define debug(x)       \
-    cerr << #x << " "; \
-    _print(x);         \
-    cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
-
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <class T, class V>
-void _print(pair<T, V> p)
+int32_t main()
 {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <class T>
-void _print(vector<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(set<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(multiset<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T, class V>
-void _print(map<T, V> v)
-{
-    cerr << "[ ";
-    for (auto i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-
-void solve()
-{
-
-    ll n;
-    cin >> n;
-    vl a(n);
-    in(a);
-    set<ll> s(all(a));
-    if (sz(s) == n)
-    {
-        out(0);
-        return;
-    }
-
-    map<ll, ll> freq;
-    for (auto x : a)
-    {
-        freq[x]++;
-    }
-
-    if (sz(a) == 3)
-    {
-        sort(all(a));
-        if (a[0] + a[1] > a[2])
-        {
-        }
-        else
-        {
-            out(0);
-            return;
-        }
-    }
-
-    ll ans = 0, first_maxi = 0, second_maxi = 0;
-
-    vpair freq_vec;
-    for (auto x : freq)
-    {
-        freq_vec.pb({x.ff, x.ss});
-    }
-
-    sort(allr(freq_vec));
-    ll count = 0;
-    for (auto it : freq_vec)
-    {
-        if (it.ss % 2 == 0)
-        {
-
-            ans += it.ss * it.ff;
-        }
-        else
-        {
-
-            ans += (it.ss - 1) * it.ff;
-        }
-    }
-
-    for (auto it : freq_vec)
-    {
-        if (it.ss % 2 == 1)
-        {
-            first_maxi = it.ff;
-            break;
-        }
-    }
-    for (auto it : freq_vec)
-    {
-        if (it.ss % 2 == 1 && it.ff != first_maxi)
-        {
-            second_maxi = it.ff;
-            break;
-        }
-    }
-
-    ans += first_maxi + second_maxi;
-    out(ans);
-}
-
-int main()
-{
-    fastio();
-
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
-#endif
-
     int t;
     cin >> t;
-    while (t--)
-        solve();
 
-#ifndef ONLINE_JUDGE
-    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
-#endif
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+
+        map<int, int> mp;
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            mp[v[i]]++;
+        }
+
+        int ans = 0;
+
+        vector<int> odd;
+
+        for (auto i : mp)
+        {
+            if (i.second % 2 == 0)
+            {
+                ans += i.second * i.first;
+            }
+
+            else
+            {
+                ans += (i.second - 1) * i.first;
+                odd.push_back(i.first);
+            }
+        }
+
+        // No Polygon
+
+        if (odd.size() == n)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+
+        // One odd
+        int m = odd.size();
+
+        int maxi = ans;
+
+        for (int i = 0; i < odd.size(); i++)
+        {
+            if (odd[i] < ans)
+            {
+                maxi = max(maxi, ans + odd[i]);
+            }
+        }
+
+        // Two odd
+        for (int i = 1; i < odd.size(); i++)
+        {
+            if (odd[i] - odd[i - 1] < ans)
+            {
+                maxi = max(maxi, ans + odd[i] + odd[i - 1]);
+            }
+        }
+
+        if (maxi == ans && odd.size() == n - 2)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+
+        cout << maxi << endl;
+    }
 }
