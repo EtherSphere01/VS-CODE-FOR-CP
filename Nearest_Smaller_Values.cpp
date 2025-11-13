b2baf244a7b4b165dc3be63475bb659a55f7a9a8
Md. Naimur Rahman <84018165+EtherSphere01@users.noreply.github.com>
2025-11-13 22:42:49 +0600

    dm

--- Nearest_Smaller_Values.cpp
+++ Nearest_Smaller_Values.cpp
@@ -1,187 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
-using namespace std;
-
-#define fastio()                      \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);                   \
-    cout.precision(numeric_limits<double>::max_digits10);
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define sp " "
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define andd &&
-#define orr ||
-#define nott !
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define allr(x) (x).rbegin(), (x).rend()
-#define mem1(a) memset(a, -1, sizeof(a))
-#define mem0(a) memset(a, 0, sizeof(a))
-#define yes cout << "YES" << nline
-#define no cout << "NO" << nline
-#define yesno(f) \
-    if (f)       \
-        yes;     \
-    else         \
-        no
-#define noyes(f) \
-    if (!f)      \
-        yes;     \
-    else         \
-        no
-#define out(x) cout << x << nline
-#define rep(i, a, b) for (ll i = a; i < b; i++)
-#define rev(i, a, b) for (ll i = a; i >= b; i--)
-#define in(a)         \
-    for (auto &x : a) \
-        cin >> x;
-#define showr(v, s, n)         \
-    for (ll i = s; i < n; i++) \
-        cout << v[i] << sp;
-#define show(v)      \
-    for (auto i : v) \
-        cout << i << sp;
-#define show_rev(v, s, n)       \
-    for (ll i = s; i >= n; i--) \
-        cout << v[i] << sp;
-#define show_pair(v, s, n)     \
-    for (ll i = s; i < n; i++) \
-        cout << v[i].ff << sp << v[i].ss << nline;
-#define vl vector<ll>
-#define vi vector<int>
-#define vc vector<char>
-#define vs vector<string>
-#define vpair vector<pair<ll, ll>>
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
-
-
-#ifndef ONLINE_JUDGE
-#define debug(x)       \
-    cerr << #x << " "; \
-    _print(x);         \
-    cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
-
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <class T, class V>
-void _print(pair<T, V> p)
-{
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <class T>
-void _print(vector<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(set<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(multiset<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T, class V>
-void _print(map<T, V> v)
-{
-    cerr << "[ ";
-    for (auto i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-
-// Policy Based Data Structures
-#ifdef __GNUC__
+// Required headers for ordered_set
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+
+// Use the namespace for pb_ds
 using namespace __gnu_pbds;
 
-typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
-template <class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-#else
-// Fallback for non-GNU compilers
-template <class T> using oset = set<T>;
-#endif
-
-
-void solve()
-{
-}
+// Define the ordered_set type
+// int: the type of data stored
+// null_type: map-like behavior (if you wanted a map, you'd use a value type here)
+// less<int>: the comparison function
+// rb_tree_tag: the tree structure to use (Red-Black Tree)
+// tree_order_statistics_node_update: policy to enable order statistics
+typedef tree<int, null_type, std::less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 int main()
 {
-    fastio();
+    ordered_set s;
 
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
-#endif
+    s.insert(10);
+    s.insert(20);
+    s.insert(5);
+    s.insert(15);
 
-    int t;
-    cin >> t;
-    while (t--)
-        solve();
+    // s is now {5, 10, 15, 20}
 
-#ifndef ONLINE_JUDGE
-    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
-#endif
+    // Find the k-th element (0-indexed)
+    // find_by_order(k): returns an iterator to the k-th element
+    std::cout << "The 2nd element (index 1) is: " << *s.find_by_order(1) << std::endl; // Output: 10
+    std::cout << "The 4th element (index 3) is: " << *s.find_by_order(3) << std::endl; // Output: 20
+
+    // Find the number of elements strictly less than a value
+    // order_of_key(x): returns the number of items strictly smaller than x
+    std::cout << "Number of elements less than 12: " << s.order_of_key(12) << std::endl; // Output: 2 (since 5 and 10 are < 12)
+    std::cout << "Number of elements less than 5: " << s.order_of_key(5) << std::endl;   // Output: 0
+
+    return 0;
 }
