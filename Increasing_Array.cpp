e7f9fa4ce4bd66843358c2578afb00f60b689239
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-11-13 21:46:05 +0600

    hotash

--- Increasing_Array.cpp
+++ Increasing_Array.cpp
@@ -5,14 +5,37 @@ class Node
 public:
     int data;
     Node *next;
+    Node *prev;
+
     Node(int val)
     {
         data = val;
-        next = nullptr;
+        next = NULL;
+        prev = NULL;
+    }
+    Node(int val, Node *nxt, Node *prv)
+    {
+        data = val;
+        next = nxt;
+        prev = prv;
     }
 };
 
-void printLL(Node *head)
+Node *convertDLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *prev = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        temp->prev = prev;
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void print(Node *head)
 {
     Node *temp = head;
     while (temp)
@@ -20,85 +43,13 @@ void printLL(Node *head)
         cout << temp->data << " ";
         temp = temp->next;
     }
-
     cout << endl;
 }
-
-Node *insert_at_head(Node *head, int data)
-{
-    Node *newNode = new Node(data);
-    if (head == nullptr)
-    {
-        return newNode;
-    }
-
-    newNode->next = head;
-    return newNode;
-}
-
-Node *insert_at_tail(Node *head, int data)
-{
-    Node *newNode = new Node(data);
-    if (head == nullptr)
-    {
-        return newNode;
-    }
-
-    Node *temp = head;
-
-    while (temp->next != nullptr)
-    {
-        temp = temp->next;
-    }
-    temp->next = newNode;
-    return head;
-}
-
-Node *insert_at_mid(Node *head, int pos, int data)
-{
-    Node *newNode = new Node(data);
-
-    if (pos == 1)
-    {
-        return insert_at_head(head, data);
-    }
-
-    int count = 0;
-    Node *temp = head;
-    Node *prev = head;
-    while (temp)
-    {
-        count++;
-        if (count == pos)
-        {
-            newNode->next = prev->next;
-            prev->next = newNode;
-        }
-        else
-        {
-            prev = temp;
-            temp = temp->next;
-        }
-    }
-
-    return head;
-}
-
 int main()
 {
-    vector<int> arr = {1, 2, 2, 3, 4, 4, 5};
-    Node *head = new Node(arr[0]);
-    int n = arr.size();
 
-    Node *temp = head;
-    for (int i = 1; i < n; i++)
-    {
-        Node *newNode = new Node(arr[i]);
-        temp->next = newNode;
-        temp = newNode;
-    }
+    vector<int> arr = {1, 2, 5, 3, 4};
 
-    printLL(head);
-    head = insert_at_mid(head, 7, 100);
-    printLL(head);
+    Node *head = convertDLL(arr);
+    print(head);
 }
