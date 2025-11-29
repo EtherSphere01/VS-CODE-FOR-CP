1467e7c604ddf07a89bd16ab8a14f813c54506ff
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-11-29 14:52:41 +0600

    hotash

---
+++ A_A.cpp
@@ -0,0 +1,291 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define sp " "
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define andd &&
+#define orr ||
+#define nott !
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define mem1(a) memset(a, -1, sizeof(a))
+#define mem0(a) memset(a, 0, sizeof(a))
+#define yes cout << "YES" << nline
+#define no cout << "NO" << nline
+#define yesno(f) \
+    if (f)       \
+        yes;     \
+    else         \
+        no
+#define noyes(f) \
+    if (!f)      \
+        yes;     \
+    else         \
+        no
+#define out(x) cout << x << nline
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define rev(i, a, b) for (ll i = a; i >= b; i--)
+#define in(a)         \
+    for (auto &x : a) \
+        cin >> x;
+#define showr(v, s, n)         \
+    for (ll i = s; i < n; i++) \
+        cout << v[i] << sp;
+#define show(v)      \
+    for (auto i : v) \
+        cout << i << sp;
+#define show_rev(v, s, n)       \
+    for (ll i = s; i >= n; i--) \
+        cout << v[i] << sp;
+#define show_pair(v, s, n)     \
+    for (ll i = s; i < n; i++) \
+        cout << v[i].ff << sp << v[i].ss << nline;
+#define vl vector<ll>
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vpair vector<pair<ll, ll>>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+void solve()
+{
+
+    ll n;
+    cin >> n;
+    vl a(n);
+    in(a);
+
+    if (n == 1)
+    {
+        if (a[0] == 0)
+        {
+            out(0);
+        }
+        else
+        {
+            out(1);
+        }
+        return;
+    }
+
+    vl temp = a;
+    sort(all(temp));
+
+    if (temp[0] == 0 && temp[n - 1] == 0)
+    {
+        out(0);
+        return;
+    }
+
+    if (n == 2)
+    {
+        if (temp[0] == 0 andd temp[1] == 0)
+        {
+            out(0);
+        }
+        else
+        {
+            out(1);
+        }
+        return;
+    }
+
+    if (n == 3)
+    {
+        if (a[0] == 0)
+        {
+            out(1);
+        }
+        else if (a[1] == 0)
+        {
+            out(1);
+        }
+        else if (a[2] == 0)
+        {
+            out(1);
+        }
+        else
+        {
+            out(2);
+        }
+
+        return;
+    }
+
+    ll left = -1, right = -1;
+    for (ll i = 0; i < n; i++)
+    {
+        if (a[i] != 0)
+        {
+            left = i;
+            break;
+        }
+    }
+
+    for (ll i = n - 1; i >= 0; i--)
+    {
+        if (a[i] != 0)
+        {
+            right = i;
+            break;
+        }
+    }
+
+    if (left == right andd left != -1)
+    {
+        if (a[left] != 0)
+        {
+            out(1);
+        }
+        else
+            out(0);
+
+        return;
+    }
+
+    vl remove;
+    for (ll i = left; i <= right; i++)
+    {
+        remove.pb(a[i]);
+    }
+
+    if (remove.size() == 0)
+    {
+        out(1);
+        return;
+    }
+    ll max_ele = remove[0];
+    ll count = 0;
+
+    for (ll i = 1; i < remove.size(); i++)
+    {
+        if (remove[i] == 0)
+            break;
+        ll diff = abs(max_ele - remove[i]);
+        diff++;
+        max_ele -= diff;
+        count++;
+        if (max_ele <= 0)
+        {
+            break;
+        }
+    }
+
+    out(count);
+}
+
+int main()
+{
+    fastio();
+
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    solve();
+
+#ifndef ONLINE_JUDGE
+    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
+#endif
+}
