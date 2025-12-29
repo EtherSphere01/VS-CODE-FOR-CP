9cbb34ca6389801e722648f59e4aa7d490246a5e
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-12-29 17:50:52 +0600

    Refactor formatting and improve logic in Book Shop solution; update problem definition

--- Book_Shop.cpp
+++ Book_Shop.cpp
@@ -1,11 +1,8 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-#define fastio()                      \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define MOD 1000000007
 #define MOD1 998244353
 #define INF 1e18
@@ -24,43 +21,25 @@ using namespace std;
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 #define allr(x) (x).rbegin(), (x).rend()
-#define mem1(a) memset(a, -1, sizeof(a))
-#define mem0(a) memset(a, 0, sizeof(a))
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
 #define yes cout << "YES" << nline
 #define no cout << "NO" << nline
-#define yesno(f) \
-    if (f)       \
-        yes;     \
-    else         \
-        no
-#define noyes(f) \
-    if (!f)      \
-        yes;     \
-    else         \
-        no
+#define yesno(f) if(f) yes; else no
+#define noyes(f) if(!f) yes; else no
 #define out(x) cout << x << nline
-#define rep(i, a, b) for (ll i = a; i < b; i++)
-#define rev(i, a, b) for (ll i = a; i >= b; i--)
-#define in(a)         \
-    for (auto &x : a) \
-        cin >> x;
-#define showr(v, s, n)         \
-    for (ll i = s; i < n; i++) \
-        cout << v[i] << sp;
-#define show(v)      \
-    for (auto i : v) \
-        cout << i << sp;
-#define show_rev(v, s, n)       \
-    for (ll i = s; i >= n; i--) \
-        cout << v[i] << sp;
-#define show_pair(v, s, n)     \
-    for (ll i = s; i < n; i++) \
-        cout << v[i].ff << sp << v[i].ss << nline;
-#define vl vector<ll>
-#define vi vector<int>
-#define vc vector<char>
-#define vs vector<string>
-#define vpair vector<pair<ll, ll>>
+#define rep(i, a, b) for(ll i=a; i<b; i++)
+#define rev(i, a, b) for(ll i=a; i>=b; i--)
+#define in(a) for (auto &x : a) cin >> x;
+#define showr(v,s,n) for(ll i=s; i<n; i++) cout<<v[i]<<sp;
+#define show(v) for(auto i:v) cout<<i<<sp;
+#define show_rev(v,s,n) for(ll i=s; i>=n; i--) cout<<v[i]<<sp;
+#define show_pair(v,s,n) for(ll i=s; i<n; i++) cout<<v[i].ff<<sp<<v[i].ss<<nline;
+#define vl vector<ll> 
+#define vi vector<int> 
+#define vc vector<char> 
+#define vs vector<string> 
+#define vpair vector<pair<ll,ll>>
 
 typedef long long ll;
 typedef unsigned long long ull;
@@ -68,132 +47,70 @@ typedef long double lld;
 // typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 
 #ifndef ONLINE_JUDGE
-#define debug(x)       \
-    cerr << #x << " "; \
-    _print(x);         \
-    cerr << endl;
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
 #else
 #define debug(x)
 #endif
 
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
 
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <class T, class V>
-void _print(pair<T, V> p)
-{
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <class T>
-void _print(vector<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(set<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(multiset<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T, class V>
-void _print(map<T, V> v)
-{
-    cerr << "[ ";
-    for (auto i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
-void solve()
-{
-    int n, x;
-    cin >> n >> x;
-    vl price(n), pages(n);
+
+void solve() {
+
+    ll n, m;
+    cin >> n >> m;
+    vl price(n);
     in(price);
+    vl pages(n);
     in(pages);
-    vector<vector<int>> dp(n + 1, vector<int>(x + 1, 0));
-
-    for (int i = 1; i <= n; i++)
-    {
-        int p = price[i - 1];
-        int pg = pages[i - 1];
-        for (int j = x; j >= 0; j--)
-        {
-            int pick = 0;
-            if (j >= p)
-            {
-                pick = dp[i - 1][j - p] + pg;
+    vector<vl> dp(n + 1, vl(m + 1, 0));
+    for (ll i = 0; i < n; i++){
+        for (ll j = 1; j <= m; j++){
+            if (j >= price[i]){
+                dp[i + 1][j] = max(dp[i][j], pages[i] + dp[i][j - price[i]]);
             }
-            int skip = dp[i - 1][j];
-            dp[i][j] = max(pick, skip);
+            else{
+                dp[i + 1][j] = dp[i][j];
+            }
         }
     }
-    // for (ll i = 0; i <= n; i++)
-    // {
-    //     for (ll j = 0; j <= x; j++)
-    //     {
-    //         cout << dp[i][j] << " ";
-    //     }
-    //     cout << nline;
-    // }
-    out(dp[n][x]);
+    out(dp[n][m]);
 }
 
-int main()
-{
+int main() {
     fastio();
 
 #ifndef ONLINE_JUDGE
     freopen("Error.txt", "w", stderr);
 #endif
 
+
+ 
     solve();
 
+
 #ifndef ONLINE_JUDGE
-    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
+    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC)*0.001 << "s\n";
 #endif
+
 }
+
+
+
