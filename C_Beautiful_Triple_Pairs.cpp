069266f096f67ce98c7ef1e07e2e8d146c6bae23
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2026-01-09 21:59:05 +0600

    Refactor C_Beautiful_Triple_Pairs.cpp: Simplify N-Queens solution by removing unused methods and streamlining recursion

--- C_Beautiful_Triple_Pairs.cpp
+++ C_Beautiful_Triple_Pairs.cpp
@@ -3,55 +3,45 @@ using namespace std;
 class Solution
 {
 private:
-    int moves[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    bool isValid(int x, int y, int n, int m)
+    bool check(int row, int col, int queens[])
     {
-        return x >= 0 && x < n && y >= 0 && y < m;
-    }
-    bool rec(vector<vector<char>> &board, string &word, int index, int x, int y, vector<vector<bool>> &visited)
-    {
-        if (index == word.size())
+        for (int i = 0; i < row; i++)
         {
-            return true;
-        }
-        int n = board.size();
-        int m = board[0].size();
-        bool ans = false;
-        for (int i = 0; i < 4; i++)
-        {
-            int newX = x + moves[i][0];
-            int newY = y + moves[i][1];
-            if (isValid(newX, newY, n, m) && !visited[newX][newY] && board[newX][newY] == word[index])
+            if (queens[i] == col || abs(queens[i] - col) == abs(i - row))
             {
-                visited[newX][newY] = true;
-                ans = ans || rec(board, word, index + 1, newX, newY, visited);
-                visited[newX][newY] = false;
+                return false;
             }
         }
-        return ans;
+        return true;
+    }
+    void rec(int level, int n, vector<vector<string>> &board, vector<string> &ans, int queens[])
+    {
+        if (level == n)
+        {
+            board.push_back(ans);
+            return;
+        }
+
+        for (int col = 0; col < n; col++)
+        {
+            if (check(level, col, queens))
+            {
+                queens[level] = col;
+                ans[level][col] = 'Q';
+                rec(level + 1, n, board, ans, queens);
+                ans[level][col] = '.';
+            }
+        }
     }
 
 public:
-    bool exist(vector<vector<char>> &board, string word)
+    vector<vector<string>> solveNQueens(int n)
     {
-        int n = board.size();
-        int m = board[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (board[i][j] == word[0])
-                {
-                    visited[i][j] = true;
-                    if (rec(board, word, 1, i, j, visited))
-                    {
-                        return true;
-                    }
-                    visited[i][j] = false;
-                }
-            }
-        }
-        return false;
+        vector<vector<string>> board;
+        vector<string> ans(n, string(n, '.'));
+        int queens[n + 5] = {0};
+        rec(0, n, board, ans, queens);
+
+        return board;
     }
 };
