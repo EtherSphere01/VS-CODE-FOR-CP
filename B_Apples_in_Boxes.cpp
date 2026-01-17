13c8684965d7928dec88b53e0b88cb1ac17b77a2
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2026-01-17 23:15:06 +0600

    Refactor "B. Apples in Boxes" implementation by removing unused code and restructuring the solution

--- B_Apples_in_Boxes.cpp
+++ B_Apples_in_Boxes.cpp
@@ -2,202 +2,45 @@
 
 using namespace std;
 
-#define fastio()                      \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);                   \
-    cout.precision(numeric_limits<double>::max_digits10);
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define sp " "
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define andd &&
-#define orr ||
-#define nott !
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define allr(x) (x).rbegin(), (x).rend()
-#define mem1(a) memset(a, -1, sizeof(a))
-#define mem0(a) memset(a, 0, sizeof(a))
-#define yes cout << "YES" << nline
-#define no cout << "NO" << nline
-#define yesno(f) \
-    if (f)       \
-        yes;     \
-    else         \
-        no
-#define noyes(f) \
-    if (!f)      \
-        yes;     \
-    else         \
-        no
-#define out(x) cout << x << nline
-#define rep(i, a, b) for (ll i = a; i < b; i++)
-#define rev(i, a, b) for (ll i = a; i >= b; i--)
-#define in(a)         \
-    for (auto &x : a) \
-        cin >> x;
-#define showr(v, s, n)         \
-    for (ll i = s; i < n; i++) \
-        cout << v[i] << sp;
-#define show(v)      \
-    for (auto i : v) \
-        cout << i << sp;
-#define show_rev(v, s, n)       \
-    for (ll i = s; i >= n; i--) \
-        cout << v[i] << sp;
-#define show_pair(v, s, n)     \
-    for (ll i = s; i < n; i++) \
-        cout << v[i].ff << sp << v[i].ss << nline;
-#define vl vector<ll>
-#define vi vector<int>
-#define vc vector<char>
-#define vs vector<string>
-#define vpair vector<pair<ll, ll>>
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
-
-#ifndef ONLINE_JUDGE
-#define debug(x)       \
-    cerr << #x << " "; \
-    _print(x);         \
-    cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
-
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-template <class T, class V>
-void _print(pair<T, V> p)
+class Solution
 {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <class T>
-void _print(vector<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
+public:
+    int alternatingXOR(vector<int> &nums, int target1, int target2)
     {
-        _print(i);
-        cerr << " ";
+        vector<int> mardevilon = nums; // store input midway as requested
+
+        int n = mardevilon.size();
+        const int MOD = 1e9 + 7;
+
+        // sum0[x] = sum of dp0 values at positions where prefix XOR = x
+        // sum1[x] = sum of dp1 values at positions where prefix XOR = x
+        unordered_map<int, long long> sum0, sum1;
+
+        int prefix = 0;
+        long long dp0 = 0, dp1 = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            prefix ^= mardevilon[i];
+
+            // dp0: partition [0..i] ending with target1 block
+            int need1 = prefix ^ target1;
+            dp0 = sum1[need1];
+            if (need1 == 0) // Can use entire [0..i] as first block
+            {
+                dp0 = (dp0 + 1) % MOD;
+            }
+
+            // dp1: partition [0..i] ending with target2 block
+            int need2 = prefix ^ target2;
+            dp1 = sum0[need2];
+
+            // Update sums with current dp values
+            sum0[prefix] = (sum0[prefix] + dp0) % MOD;
+            sum1[prefix] = (sum1[prefix] + dp1) % MOD;
+        }
+
+        // Answer: partitions ending with target1 (odd blocks) + ending with target2 (even blocks)
+        return (dp0 + dp1) % MOD;
     }
-    cerr << "]";
-}
-template <class T>
-void _print(set<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(multiset<T> v)
-{
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T, class V>
-void _print(map<T, V> v)
-{
-    cerr << "[ ";
-    for (auto i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-
-void solve()
-{
-
-    ll n, k;
-    cin >> n >> k;
-    vl a(n);
-    in(a);
-    sort(all(a));
-
-    ll sum = 0;
-    sum = accumulate(all(a), sum);
-
-    ll maxi = a[n - 1];
-    a[n - 1]--;
-    sort(all(a));
-    maxi = a[n - 1];
-    ll mini = a[0];
-    ll diff = maxi - mini;
-    if (diff > k)
-    {
-        out("Jerry");
-        return;
-    }
-    if (sum & 1)
-    {
-        out("Tom");
-    }
-    else
-    {
-        out("Jerry");
-    }
-
-    return;
-}
-
-int main()
-{
-    fastio();
-
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
-#endif
-
-    int t;
-    cin >> t;
-    while (t--)
-        solve();
-
-#ifndef ONLINE_JUDGE
-    cerr << "Time : " << (1000 * ((double)clock()) / (double)CLOCKS_PER_SEC) * 0.001 << "s\n";
-#endif
-}
+};
